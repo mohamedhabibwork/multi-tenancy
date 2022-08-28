28cd1ad8b1df8aabc2b4e3dfe6231d53aaea3c15
mohamedhabibwork <64292519+mohamedhabibwork@users.noreply.github.com>
2022-08-28 21:44:51 +0200

    domain

--- routes/domain.php
+++ routes/domain.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', static function ($account){
-   return "{$account} web";
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 });
